b9777d35e40ca167e861ea456086f065871ad81b
Angelika Modrzejewska <ang.modrzejewska@gmail.com>
2020-06-29 14:27:27 +0200

    Updates

---
+++ precache-manifest.2c7d6378fe803951030039a446d07aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c56053abd19947752de5",
+    "url": "/react-to-do-list/static/css/main.fc3d06b1.chunk.css"
+  },
+  {
+    "revision": "c56053abd19947752de5",
+    "url": "/react-to-do-list/static/js/main.c56053ab.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/react-to-do-list/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "52435d08ff1beb8e480e",
+    "url": "/react-to-do-list/static/js/runtime~main.52435d08.js"
+  },
+  {
+    "revision": "01ff0380e3998c9b4c3d9cb4b4e4cfec",
+    "url": "/react-to-do-list/index.html"
+  }
+];
